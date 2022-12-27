688ed9a7e1a18d94cf7775cd787d15aa31d5f9fc
Victor-Delgado-M <94375075+Victor-Delgado-M@users.noreply.github.com>
2022-12-27 15:56:16 -0500

    Add files via upload

---
+++ almacenamiento de registro base de datos/src/firebaseConfig.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+};
+
+initializeApp(firebaseConfig);
+const auth = getAuth();
+
+export {auth};
